8f0f2823fea29aeb0e5e1675a4eeda4f8ebdd56c
Heather Condron <hcondro@wgu.edu>
2020-02-09 12:48:29 -0500

    create search-box component and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import { CardList } from './components/card-list/card-list.component.jsx';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox} from './components/search-box/search-box.component';
 
 
 class App extends Component {
@@ -9,26 +10,35 @@ class App extends Component {
 
     this.state = {
      monsters: [],
-     searchField: []
+     searchField: '',
     };
   }
+
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response =>  response.json())
       .then(users =>  this.setState({ monsters: users}));
     }
+
+    handleChange= (e) => {
+      this.setState({ searchField: e.target.value });
+    }
+      
  
   render() {
+    const { monsters, searchField }  = this.state;
+    const filteredMonsters= monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return (
       <div className='App'>
-        <input 
-        type="search" 
-        placeholder="search monsters" 
-        onchange={e => 
-          this.setState ({ searchField: e.target.value})}
+        <SearchBox
+        placeholder='search monsters'
+        handleChange= {this.handleChange}
         />
-        <CardList monsters= {this.state.monsters}/>
+
+        <CardList monsters= { filteredMonsters }/>
       </div>
     );
   }
